42f93f6655a3f1a47c0550ba592c49703caf63e1
Jordan Martinez <jordanalex.martinez@protonmail.com>
2021-09-17 20:34:54 -0700

    Verify that use actually display something

--- test/Main.purs
+++ test/Main.purs
@@ -7,6 +7,7 @@ import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Halogen (Component)
 import Halogen.Aff (selectElement)
+import Halogen.HTML.Properties as HP
 import Halogen.Hooks as Hooks
 import Halogen.Svg.Attributes (Color(..))
 import Halogen.Svg.Attributes as SA
@@ -29,10 +30,23 @@ rootComp = Hooks.component \_ _ -> Hooks.do
   Hooks.pure do
     SE.svg
       [ SA.viewBox 0.0 0.0 30.0 10.0 ]
+      -- https://developer.mozilla.org/en-US/docs/Web/SVG/Element/use
       [ SE.circle
-          [ SA.cx 5.0
+          [ HP.id "circleId"
+          , SA.cx 5.0
           , SA.cy 5.0
           , SA.r 4.0
           , SA.stroke $ Named "blue"
+          ]
+      , SE.use
+          [ SA.href "#circleId"
+          , SA.x 10.0
+          , SA.fill $ Named "blue"
+          ]
+      , SE.use
+          [ SA.href "#circleId"
+          , SA.x 20.0
+          , SA.fill $ Named "white"
+          , SA.stroke $ Named "red"
           ]
       ]
